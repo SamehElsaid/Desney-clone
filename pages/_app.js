753e8c490af19fe09b36e6de028e90b858818d18
unknown <samehmeligy3629@gmail.com>
2022-12-25 11:36:20 +0200

    Edit Logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import { Provider } from "react-redux";
 import store from "../Redux/Store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <Head>
+        
+      </Head>
       <Component {...pageProps}  />
     </Provider>
   );
